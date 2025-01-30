9fa40ac6cdd6237dbdf3358daea2f7958145ecf7
Trevor <trevorjloth@gmail.com>
2025-01-29 23:39:26 -0500

    Update main.py
    - Fixed a clarity bug where dealer's new card was misidentified, even though the math and value were correct.
    - Added a card counting toggle option.

--- main.py
+++ main.py
@@ -13,10 +13,9 @@ NEW_DECK = ['CA', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C1', 'CJ', 'C
             'SA', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S1', 'SJ', 'SQ', 'SK']
 
 def hit(hand, d, rt):
-    
     card = d.pop()
-    count_card(rt, card)
     hand.append(card)
+    count_card(rt, card)
     result = calc_hand(hand)
     return len(result) > 0
 
@@ -45,7 +44,7 @@ def bet(players, index):
 def reshuffle(d, p_hands, d_hand):
     # Reshuffle deck excluding in-play cards
     if len(d) < 2* (len(p_hands) + 1): # 2 cards per player + 1 for the dealer hand
-        print("Shuffling remainin cards.")
+        print("\033[33mShuffling remaining cards.\033[0m")
         d = NEW_DECK[:]
         random.shuffle(d)
         for i in p_hands:
@@ -57,14 +56,26 @@ def reshuffle(d, p_hands, d_hand):
 
 def count_card(rt, card):
     if "2" in card or "3" in card or "4" in card or "5" in card or "6" in card:
-        rt += 1
+        rt[0] += 1
     elif "A" in card or "K" in card or "Q" in card or "J" in card or "1" in card:
-        rt -= 1
+        rt[0] -= 1
     return rt
 
 
 if __name__ == "__main__":
-    print("\033[34m==============================\nWelcome to Blackjack!\nThis is Version 1.1\n------------------------------\nWhats New?\n- Added color for visual clarity\n------------------------------\nContacts \"steraks\" on discord for queries\n==============================\n\033[0m")
+    print(
+    "\033[34m==============================\n" + 
+    "Welcome to Blackjack!\nThis is Version 1.2\n" + 
+    "------------------------------" + 
+    "\nWhats New?\n" +
+    "- Added color for visual clarity\n" + 
+    "- Fixed a clarity bug where dealer's new card was misidentified, even though the math and value were correct." +
+    "- Added a card counting toggle option." +
+    "------------------------------\n" + 
+    "Contacts \"steraks\" on discord for queries\n" + 
+    "==============================\n\033[0m"
+    )
+    
     print(
         "⢠⣾⡿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣆\n" +
         "⣿⠃⠀⠀⠀⠘⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀\n" +
@@ -87,8 +98,10 @@ if __name__ == "__main__":
         playing_deck = NEW_DECK[:]
         random.shuffle(playing_deck)
         num_of_players = 0
-        running_total = 0
+        running_total = [0]
+        show_count = None
 
+        # Get desired number of players
         while num_of_players < 1 or num_of_players > 8:
             try:
                 num_of_players = int(input("\033[32mEnter number of players between 1 and 8.\033[0m\n"))
@@ -99,6 +112,21 @@ if __name__ == "__main__":
 
             except:
                 print("Invalid number of players! Try again.")
+        
+        # Get player pref to see card count
+        while show_count == None:
+            try:
+                tmp = input("\033[32mWould you like to see the card count? Y/N?\033[0m\n")
+                if tmp.upper() == "Y":
+                    show_count = True
+                    break
+                elif tmp.upper() == "N":
+                    show_count = False
+                    break
+                else:
+                    ValueError
+            except:
+                print("Don't be a rat.", end="")
         
         player_chips = [[30, 0] for x in range(num_of_players)] # [total chips, active bet] if None, player is out
 
@@ -112,6 +140,8 @@ if __name__ == "__main__":
 
             player_turn = True
 
+            playing_deck = reshuffle(playing_deck, player_hands, dealer_hand)
+
             # Place bets before cards are dealth
             for num in range(num_of_players):
                 if player_chips[num] == None:
@@ -124,13 +154,17 @@ if __name__ == "__main__":
                 for j in range(len(player_hands)):
                     if player_chips[j] == None:
                         continue
-                    card = playing_deck.pop()
-                    count_card(running_total, card)
-                    player_hands[j].append(card)
-                card = playing_deck.pop()
-                count_card(running_total, card)
-                dealer_hand.append(card)
+                    player_hands[j].append(playing_deck.pop())
+                dealer_hand.append(playing_deck.pop())
             
+            # count player cards
+            for num in range(len(player_hands)):
+                for j in range(len(player_hands[num])):
+                    card = player_hands[num][j]
+                    count_card(running_total, card)
+            # count dealer 1st card
+            count_card(running_total, dealer_hand[0])
+
             d_calc = calc_hand(dealer_hand)
 
             playing_deck = reshuffle(playing_deck, player_hands, dealer_hand)
@@ -146,16 +180,16 @@ if __name__ == "__main__":
                 print("==============================")
                 if dealer_hand[0][1] == "A":
                     print(f"Dealer has {deck[dealer_hand[0]]} and a hidden card. " +
-                    f"\n\033[36mMin Value: 2\033[0m, " +
+                    f"\n\033[36mMin Value: 12\033[0m, " +
                     f"\n\033[36mMax Value: 21\033[0m\n------------------------------")
                 else:
                     tmp = calc_hand([dealer_hand[0]])[0]
                     print(f"Dealer has {deck[dealer_hand[0]]} and a hidden card. " +
-                        f"\n\033[36mMin hand value: {tmp + 1} \033[0m" +
+                        f"\n\033[36mMin hand value: {tmp + 2} \033[0m" +
                         f"\n\033[36mMax hand value: {tmp + 11}\033[0m\n------------------------------")
 
                 while player_turn:
-                    try:                                        
+                    try:
                         # Show Player Hand
                         print(f"Player number {num+1} hand is ", end="")
                         for i in range(len(player_hands[num])-1):
@@ -169,7 +203,11 @@ if __name__ == "__main__":
                             for i in range(1, len(hand_vals)):
                                 print(" or\033[36m", hand_vals[i], end="\033[0m")
                             print(".")
+                        print()
                         
+                        if show_count:
+                            print("\033[36mRunning total:", running_total[0], "\033[0m")
+
                         if max(hand_vals) == 21:
                             print("Player", num+1, "got Blackjack!")
                             player_turn = False
@@ -177,13 +215,12 @@ if __name__ == "__main__":
                             break
 
                         time.sleep(0.5)
-                        print()
                         user_input = int(input("\033[32mHit or Stand? 1 for Hit, 2 for Stand, 3 to exit...\033[0m\n"))
 
                         if user_input == 1:
                             print("Player", num+1, "hit.")
                             
-                            hit_res = hit(player_hands[num], playing_deck)
+                            hit_res = hit(player_hands[num], playing_deck, running_total)
 
                             print(f"Player number {num+1} drew a {deck[player_hands[num][-1]]}. New \033[36mvalue: ", end="")
                             p_hand = calc_hand(player_hands[num])
@@ -219,10 +256,14 @@ if __name__ == "__main__":
             # Stand on 17 or higher: The dealer must always stand when their hand value reaches 17 or more.
             # Hit on 16 or less: If the dealer's hand totals 16 or less, they must take another card (hit).
             # No choice to "beat" players: The dealer's actions are dictated by the rules, not by the goal of beating each player at the table.
+            
+            # count dealer 2nd card which was not counted originially
+            count_card(running_total, dealer_hand[1])
 
             while True:
                 time.sleep(0.5)
                 dealer_stand = False
+
                 # Show dealer Hand
                 print(f"Dealer hand is ", end="")
                 for i in range(len(dealer_hand)-1):
@@ -234,6 +275,9 @@ if __name__ == "__main__":
                         print(" or\033[36m", d_calc[i], end="\033[0m")
                     print(".")
                 print()
+                
+                if show_count:
+                    print("\033[36mRunning total:", running_total[0], "\033[0m")
 
                 time.sleep(0.5)
                 if max(d_calc) >= 17:
@@ -242,10 +286,10 @@ if __name__ == "__main__":
                 if not dealer_stand:
                     print("Dealer must hit!")
                     time.sleep(0.5)
-                    hit_res = hit(dealer_hand, playing_deck)
+                    hit_res = hit(dealer_hand, playing_deck, running_total)
                     time.sleep(0.5)
                     # Show dealer's draw
-                    print(f"Dealer drew a {deck[player_hands[num][-1]]}. New\033[36m value: ", end="")
+                    print(f"Dealer drew a {deck[dealer_hand[-1]]}. New\033[36m value: ", end="")
                     d_calc = calc_hand(dealer_hand)
                     print(str(max(d_calc)) + "\033[0m" if hit_res else "BUST!\033[0m") # Print the new Value
 
@@ -258,6 +302,9 @@ if __name__ == "__main__":
                             if not player_hands_bust[num]:
                                 # payout to players
                                 player_chips[num][0] += player_chips[num][1]*2
+                        
+                        if show_count:
+                            print("\033[36mRunning total:", running_total[0], "\033[0m")
                         break
                 else:
                     d_max = max(d_calc) # Print the value
@@ -304,14 +351,14 @@ if __name__ == "__main__":
                 for i in winners:
                     time.sleep(0.5)
                     print(F"\033[35mPlayer {i+1} won with {player_chips[i][0]} chips!\033[0m")
-                a = input("\033[32mPlay again? Y/N\033[0m\n")
+                a = input("\033[32mPlay again? Y/N?\033[0m\n")
                 if a.upper() == "Y":
                     break
                 sys.exit()
             
             if not active_players:
                 print("\033[33mAll players ran out of chips. The house wins!\033[0m")
-                a = input("\033[32mPlay again? Y/N\033[0m\n")
+                a = input("\033[32mPlay again? Y/N?\033[0m\n")
                 if a.upper() == "Y":
                     break
                 sys.exit()
